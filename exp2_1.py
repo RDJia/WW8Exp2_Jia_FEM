25f7930afd5719651f67548d8521a180578c1738
RDJia <112781726+RDJia@users.noreply.github.com>
2024-12-10 13:51:00 +0100

    Update10.12.24 exp2_1.py

--- exp2_1.py
+++ exp2_1.py
@@ -325,8 +325,7 @@ class Integration():
         print(f'integral of n={n}, d={d}:  ', quad_integration)
         return quad_integration
 
-    @staticmethod
-    def NewtonCotes(x1, x2, n):
+    def NewtonCotes(self, x1, x2, n):
         '''
         Create lambda and zeta given degree n.
         :param x1:  float,  starting point of integral
@@ -361,52 +360,32 @@ class Integration():
         return intg
 
 class LinearSys():
-    def linear_shape_functions(self, xi):
-        """
-        linear shape function
-        :param xi: Evaluation point in the reference domain [-1, 1]
-                   xi=0: center, xi=-1: left end, xi=1: right end.
-        :return: Array of shape functions [N1, N2]
-        """
-        N1 = 0.5 * (1 - xi)
-        N2 = 0.5 * (1 + xi)
-        return np.array([N1, N2])
 
-    @staticmethod
-    def linear_shape_function_derivatives(Le):
+    def oneElement(self, x1=0, x2=50, E=210000, A=25, l=50, f_b=0, f_s=5):
         '''
-        Calculate the derivatives of linear shape functions in the reference domain.
-        :param Le: float.
-        :return: Array of shape function derivatives [dN1_dxi, dN2_dxi]
+        Special case for Task 5_3: one element with BC on first node (x=0, u=0).
+        Test 1 element given following values.
+        :param x1, x2:  float,  starting/ending position (mm)
+        :param E:  float,  E-Modul (N/mm^2)
+        :param A:  float,  cross section area (mm^2)
+        :param l:  float,  element length (mm)
+        :param f_b, f_s:  float,  force on starting/ending point (N)
         '''
-        dN1_dxi = -1/Le
-        dN2_dxi = 1/Le
-        return np.array([dN1_dxi, dN2_dxi])
 
-    def element_stiffness_matrix(self, E, A, Le):
-        """
-        Assemble element stiffness matrix based on linear shape func and numerical integration.
-        :param E, A, Le: float, Young’s Modulus, sectional area and element length.
-        :return: Element stiffness matrix Ke
-        """
-        B = self.linear_shape_function_derivatives(Le).reshape(1,2)
-        # print("B:\n", B)
-        # print(B.shape)
-        # print("B.T:\n", B.T)
-        #print(B.T.shape)
-        Ke = E * A * (B.T @ B) * Le
+        Ke = E * A * 2 * l / (x1-x2)**2
+        u2 = (f_b+f_s) / Ke
 
-        return Ke
+        print('displacement of ending point ≈', np.round(u2, 7))
 
-    def assembleLinear(self, Ke=None, N = 5, u1 = 1.0, uend = 5.0, f_b = -5, f_s = 5, to_print = True):
+    def assembleLinear(self, c = 1, N = 5, u1 = 1.0, uend = 5.0, f_b = -5, f_s = 5, to_print = True):
         '''
         * Task 6.2: assembly routine of stiffness matrix for linear elements.
         * The element stiffness matrix Ke = np.array([[1,-1],[-1,1]])
           indicates a linear relationship of values between neighbouring elements.
         * The global stiffness matrix is assembled by adding up element
           stiffness matricesin diagonal direction.
-        :param Ke:  ndarray, element stiffness matrix.
-        :param N:  int, number of elements.
+        :param c:  float, stiffness multiplier.
+        :param N:  int, number of nodes.
         :param u1:      float, displacement on first node (Dirichlet BC)
         :param uend:    float, displacement on last node (Dirichlet BC)
                         = None if no Dirichlet BC on it
@@ -414,45 +393,36 @@ class LinearSys():
         :param to_print:    boolean, whether to print the parameters
         :return K: ndarray, global stiffness matrix.
         '''
-        # Check element stiffness matrix.
-        if Ke is None:
-            raise ValueError("The parameter Ke (element stiffness matrix) must be provided.")
-        else:
-            print("Assembling global stiffness matrix with Ke=\n", Ke)
         # fill the global stiffness matrix
-        K = np.zeros((N+1, N+1))
-        for ele in range(N):
-            # Global node indices for the current element
-            node1 = ele
-            node2 = ele + 1
-
-            # Assemble the local Ke into the global K
-            K[node1, node1] += Ke[0, 0]
-            K[node1, node2] += Ke[0, 1]
-            K[node2, node1] += Ke[1, 0]
-            K[node2, node2] += Ke[1, 1]
-
+        K = np.zeros((N, N))
+        for ele in range(N-1):
+            K[ele][ele] += 1
+            K[ele][ele + 1] -= 1
+            K[ele + 1][ele] -= 1
+            K[ele + 1][ele + 1] += 1
             if to_print == True:
                 print(f'--------# element {ele} #:--------')
                 print('###Globall stiffness matrix### :\n', K)
 
         # generate displacement vector u
-        u = np.full((N+1,1),'unknown')
+        u = np.full((N,1),'unknown')
         u[0] = u1
         if uend is not None:
             u[-1] = uend
         # right hand side
-        rhs = np.zeros((N+1,1))
+        rhs = np.zeros((N,1))
         rhs[0], rhs[-1] = f_b, f_s
 
         if to_print == True:
             print('###displacement vector### :\n', u)
             print('--------# left hand side of the equation #--------')
-            print(f'{Ke}\n · {u}')
+            print(f'{c} * {K}\n · {u}')
             print('--------# right hand side of the equation #--------')
             print(f'f_b + f_s = \n{rhs}')
 
-        return K
+        print('###K*c### :\n', K*c)
+
+        return K*c
 
     def calc_c(self, A, E, L):
         '''
@@ -487,7 +457,7 @@ class LinearSys():
                         P[row,col] = 1
         return P
 
-    def deleteRow(self, originMat, uend=None, axis=0):
+    def deleteRow(self, originMat, uend = None, axis=0):
         '''
         * Delete row(s)/column(s) in the linear equations where
           the nodal values are already given by Dirichlet BCs.
@@ -497,76 +467,81 @@ class LinearSys():
         :param axis: int, = 0 to remove row, = 1 to remove column
         :return reducedMat: ndarray, reduced matrix.
         '''
-        # print('originMat: ', originMat)
-        # print('shape:', np.shape(originMat))
+        #print('originMat: ', originMat)
+        #print('shape:', np.shape(originMat))
         reducedMat = np.delete(originMat, 0, axis=axis)
-        # print('reducedMat after deleting first element:', reducedMat)
-        if uend is not None:  # for Dirichlet BCs at both sides.
+        #print('reducedMat:', reducedMat)
+        if uend is not None:        # for Dirichlet BCs at both sides.
             reducedMat = np.delete(reducedMat, -1, axis=axis)
-            # print('reducedMat after potentially deleting last element:', reducedMat)
         return reducedMat
 
-    def solveLinear(self, N=6, Ke=None, u1=0, uend=None, F=12, to_print=True):
+    def solveLinear(self, N = 6, u1 = 0, uend = None, F = -5, c = 1, to_print = True):
         '''
         * Solve linear equations for given conditions,
           acquire displacement result on the nodes.
         * Reduced matrices are created by deleting
           rows and columns.
         :param N:       int, number of elements
-        :param Ke:  ndarray, element stiffness matrix.
         :param u1:      float, displacement on first node (Dirichlet BC)
         :param uend:    float, displacement on last node (Dirichlet BC)
                         = None if no Dirichlet BC on it
         :param F:       float, force applied on the last node
+        :param c:       float, multiplier of global stiffness matrix
         :param to_print:    boolean, whether to print the parameters
         :return d:      ndarray, solution, displacement matrix
         '''
+
         if to_print == True:
             print('==============================')
-            print('Testing: N =', N, ', u1 =', u1, ', uend =', uend, ', F =', F, ', Ke =\n', Ke)
+            print('Testing: N =', N, ', u1 =', u1, ', uend =', uend, ', F =', F, ', c =', c )
 
         # Create global stiffness matrix.
-        K = self.assembleLinear(N=N, Ke=Ke, u1=u1, uend=uend, f_b=0, f_s=F, to_print=False)
-
-        # Modify the stiffness matrix and load vector to account for Dirichlet BCs
+        K = self.assembleLinear(N=N, c=c, u1=u1, uend=uend, f_b = 0, f_s = F, to_print = False)
         Kr = self.deleteRow(K, uend=uend, axis=1)
         Kr = self.deleteRow(Kr, uend=uend, axis=0)
+        print("Kr:\n", Kr)
 
-        d_dirichlet = np.zeros([N + 1, 1])
+        d_dirichlet = np.zeros([N,1])
         d_dirichlet[0] = u1
         if uend is not None:
             d_dirichlet[-1] = uend
-
+        print('=== d_dirichlet ===\n', d_dirichlet)
         # reduced right hand side
-        f = np.zeros([N + 1, 1])
+        f = np.zeros([N, 1])
         f[-1] = F
-        if uend is not None:
-            f[0] -= K[0, -1] * uend
-            f[-1] -= K[-1, 0] * u1
+        f[0] = - F
 
         fr = self.deleteRow(f, uend=uend, axis=0)
+        fr[0] = u1
+        print("fr:\n", fr)
+        if uend is not None:
+            fr[-1] = uend
+            print("fr:\n", fr)
 
         # calculate displacement vector
-        dr = scipy.linalg.solve(Kr, fr)
-
+        dr = scipy.linalg.solve(Kr, fr) * c
+        print('=== dr ===\n', dr)
         if uend is not None:
-            d = np.zeros([N + 1, 1])
-            for element in range(N - 1):
-                d[element + 1] = dr[element]
-            d[0] = u1
-            d[-1] = uend
+            d = np.zeros([N,1])
+            for element in range(N-2):
+                d[element+1] = dr[element]
         else:
             d = np.insert(dr, 0, 0, axis=0)
-            d += u1
-
-        # If both u1 and uend are defined, adjust the intermediate nodes linearly
-        if uend is not None:
-            for i in range(1, N):
-                d[i] = u1 + i * (uend - u1) / N
+        d = d + d_dirichlet
         print('=== d ===\n', d)
+
+        '''if to_print == True:
+            print('=== d ===\n', d)
+            print('=== K ===\n', K)
+            print('=== d_dirichlet ===\n', d_dirichlet)
+            print('=== f ===\n', f)
+            print('=== fr ===\n', fr)
+            print('=== Kr ===\n', Kr)
+            print('=== dr ===\n', dr)'''
+
         return d
 
-    def postProcess(self, BC_1, A, E, L, F, N ):
+    def postProcess(self, BC_1, A, E, L, F, N=6):
         '''
         Post process:
         Calculate and plot stress tensors under different BCs.
@@ -580,27 +555,25 @@ class LinearSys():
         :return x_points: ndarray, indicating
         '''
         d_1 = []    # displacements
-        num_bc = np.shape(BC_1)[0]  # number of BCs
-        print('num_bc=',num_bc)
+        N_BCs = np.shape(BC_1)[0]  # number of BCs
+        N = N   # number of elements
+
+        print('number of BCs =',N_BCs)
+        print('number of elements =', N)
+
         # Assemble the system based on given BC, solve the displacements.
-        for BC in range(N):
-            Ke = self.element_stiffness_matrix(E=E, A=A, Le=L/N)
-            d_1.append(self.solveLinear(N=N, Ke=Ke, u1=BC_1[BC][0], uend=BC_1[BC][1], F=F, to_print=False))
+        for BC in range(N_BCs):
+            d_1.append(self.solveLinear(N=N, u1=BC_1[BC][0], uend=BC_1[BC][1], F=F, c=A*E/L, to_print=False))
         print('---all elements of d_1:---\n', d_1)
 
         stress_fields = []  # list of all stress fields
-        x_points = np.linspace(1, N+1, N+1) # nodes
-        x_elements = np.linspace(1, N, N)  # elements
+        x_points = np.linspace(1, N, N) # nodes
+
         # Calculate gradient for on displacement results based on different BCs.
-        for BC in range(num_bc):
-            print('BC:', BC)
-            grad_1 = np.zeros(N+1)
-            d_element = np.zeros(N+1)      # local displacement in element
-            for i in range(N):
-                print('i:', i)
-                print('N:', N)
-                print('grad_1[i + 1]:', grad_1[i + 1])
-                print('d_1[BC][i + 1]:', d_1[BC][i + 1])
+        for BC in range(N_BCs):
+            grad_1 = np.zeros(N)
+            d_element = np.zeros(N)      # local displacement in element
+            for i in range(N-1):
                 grad_1[i + 1] = d_1[BC][i + 1] - d_1[BC][i][0]
                 d_element[i + 1] = d_1[BC][i + 1] - d_1[BC][i][0]
             grad_1[0] = grad_1[1]
@@ -642,12 +615,10 @@ class LinearSys():
             ax_g.set_title(f'gradient of u1 = {BC_1[BC][0]} mm, uend = {BC_1[BC][1]} mm')
             ax_g.set_ylabel('gradient')
 
-            plt.suptitle(f'u1 = {BC_1[BC][0]} mm, uend = {BC_1[BC][1]} mm\n N = 6, F = 12 N, c = 1')
+            plt.suptitle(f'u1 = {BC_1[BC][0]} mm, uend = {BC_1[BC][1]} mm\n N = {N}, F = 12 N, c = 1')
             plt.show()
 
-        print("stress_fields shape:", len(stress_fields))
-        print("x_points shape:", x_points.shape)
-        return stress_fields, x_points, x_elements
+        return stress_fields, x_points
 
             
 
